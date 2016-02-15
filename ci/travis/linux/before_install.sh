b1d621e9ea7b414d0474d3bd4a391fba1200cb64
Nicolas Fortin <github@nettrader.fr>
2016-02-15 16:57:50 +0100

    Update travis build

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -6,5 +6,20 @@ sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
         libboost-thread-dev \
         libboost-random-dev \
         libboost-system-dev \
-		swig
+	libboost-python-dev \
+	freeglut3-dev \
+	libxmu-dev \
+        libxi-dev \
+	swig \
+	libpng-dev \
+	libjpeg-dev \
+	libxxf86vm1 \
+	libxxf86vm-dev \
+	libxi-dev \
+	libxrandr-dev \
+	mesa-common-dev \
+	mesa-utils-extra \
+        libgl1-mesa-dev \
+        libglapi-mesa
+	
 
