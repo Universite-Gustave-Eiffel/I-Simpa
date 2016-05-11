31268437333929758304bb1b97d1059405aa2bed
Austil <augustin@mrlacour.fr>
2016-05-11 16:38:55 +0200

    Directivity parser done
    - test case with full code coverage
    - no more int, every value are use double

--- src/lib_interface/directivityBalloon.h
+++ src/lib_interface/directivityBalloon.h
@@ -35,9 +35,6 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/regex.hpp>
 
-#include <boost/iostreams/device/mapped_file.hpp>
-#include <boost/iostreams/stream.hpp>
-
 using namespace std;
 
 /**
@@ -49,12 +46,14 @@ public:
 	t_DirectivityBalloon(string filePath);
 	~t_DirectivityBalloon();
 
-	bool asDataForFrequency(int frequency);
-	float getInterpolatedValue(int freq, float phi, float theta);
+	bool asDataForFrequency(double frequency);
+	bool asValue(double freq, double phi, double theta);
+	double getValue(double freq, double phi, double theta);
+	double getInterpolatedValue(double freq, double phi, double theta);
 
 private:
 	// magnitude values as attenuations[frequency][phi][theta] = value
-	std::map<int, std::map<int, std::map<int, float>>> attenuations;
+	std::map<double, std::map<double, std::map<double, double>>> attenuations;
 };
 
 //--------------------------------------------------------------------------------------------------------------------\\
@@ -67,7 +66,7 @@ t_DirectivityBalloon::t_DirectivityBalloon(string filePath)
 	}
 
 	string line;
-	int currentFrequency {0};
+	double currentFrequency {0.0};
 	vector< string > tokens;
 	boost::regex re("[^0-9]");
 
@@ -75,25 +74,24 @@ t_DirectivityBalloon::t_DirectivityBalloon(string filePath)
 	{
 		if (!line.empty() && line[0] != ';') // not a comment
 		{
-			boost::split(tokens, line, boost::is_any_of(",")); // tokenize, 60% of cpu time
+			boost::split(tokens, line, boost::is_any_of(",")); // split, 60% of cpu time
 			if (!tokens.empty())
 			{
 				// frequency
-				if (tokens.size() == 3 && boost::equals(tokens[0], "\"Frequency\"")) // use iequals() for case insensitivity
+				if (boost::equals(tokens[0], "\"Frequency\"") && tokens.size() == 3) // use iequals() for case insensitivity
 				{
-					currentFrequency = stoi(tokens[1]);
+					currentFrequency = stod(tokens[1]);
 				}
-				// data
+				// data, 40% of cpu time
 				else if (tokens.size() == 38 && currentFrequency > 0)
 				{
-					// boost::trim(tokens[0]);
 					boost::erase_all_regex(tokens[0], re);
-					int phi = stoi( tokens[0] );
+					double phi = stod( tokens[0] );
 
 					for (auto i = 1; i < tokens.size(); i++)
 					{
-						int theta = (i-1) * 5;
-						float value = stol(tokens[i]);
+						double theta = (i-1) * 5;
+						double value = stod(tokens[i]);
 						attenuations[currentFrequency][phi][theta] = value;
 					}
 				}
@@ -110,13 +108,44 @@ t_DirectivityBalloon::~t_DirectivityBalloon()
 {
 }
 
-bool t_DirectivityBalloon::asDataForFrequency(int frequency)
+bool t_DirectivityBalloon::asDataForFrequency(double frequency)
 {
 	return !(attenuations.find(frequency) == attenuations.end());
 }
 
-float t_DirectivityBalloon::getInterpolatedValue(int freq, float phi, float theta)
+bool t_DirectivityBalloon::asValue(double freq, double phi, double theta)
 {
-	// TODO
-	return 0.0;
+	if (!this->asDataForFrequency(freq))
+		return false;
+	if (attenuations[freq].find(phi) == attenuations[freq].end())
+		return false;
+	if (attenuations[freq][phi].find(theta) == attenuations[freq][phi].end())
+		return false;
+
+	return true;
+}
+
+double t_DirectivityBalloon::getValue(double freq, double phi, double theta)
+{
+	return attenuations[freq][phi][theta];
+}
+
+double t_DirectivityBalloon::getInterpolatedValue(double freq, double phi, double theta)
+{
+	double phi_lowBound, phi_upBound, theta_lowBound, theta_upBound;
+
+	phi_upBound = attenuations[freq].upper_bound(phi)->first;
+	phi_lowBound = phi_upBound - 5;
+	theta_upBound = attenuations[freq][phi_lowBound].upper_bound(theta)->first;
+	theta_lowBound = theta_upBound - 5;
+
+	// TODO : bilinear interpolation (but we're on an sphere so it's probably a mistake)
+
+	float u = (theta - theta_lowBound) / (theta_upBound - theta_lowBound);
+	float t = (phi - phi_lowBound) / (phi_upBound - phi_lowBound);
+
+	float va = (1-u) * attenuations[freq][phi_lowBound][theta_lowBound] + u * attenuations[freq][phi_lowBound][theta_upBound];
+	float vb = (1-u) * attenuations[freq][phi_upBound][theta_lowBound] + u * attenuations[freq][phi_upBound][theta_upBound];
+
+	return (1-t) * va + t * vb;
 }
--- src/lib_interface/tests/balloon_test.cpp
+++ src/lib_interface/tests/balloon_test.cpp
@@ -1,20 +1,60 @@
-#define BOOST_TEST_MODULE lib_interface test
+#define BOOST_TEST_MODULE lib_interface balloon tests
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
 #include <directivityBalloon.h>
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE( constructor_test1 )
+BOOST_AUTO_TEST_CASE(constructor_test1)
 {
-	t_DirectivityBalloon balloon1("speaker-test1.txt");
-	BOOST_TEST(balloon1.asDataForFrequency(100) == true);
-	BOOST_TEST(balloon1.asDataForFrequency(200) == false );
+	t_DirectivityBalloon balloon("speaker-test1.txt");
+
+	BOOST_TEST(balloon.asDataForFrequency(100)	== true);
+	BOOST_TEST(balloon.asDataForFrequency(200)	== false);
+
+	BOOST_TEST(balloon.asValue(100, 45, 90)		== true);
+	BOOST_TEST(balloon.asValue(100, 355, 170)	== true);
+
+	BOOST_TEST(balloon.asValue(200, 45, 90)		== false);
+	BOOST_TEST(balloon.asValue(200, 355, 170)	== false);
+	BOOST_TEST(balloon.asValue(100, 48, 90)		== false);
+	BOOST_TEST(balloon.asValue(100, 400, 170)	== false);
+	BOOST_TEST(balloon.asValue(100, 45, 2)		== false);
+	BOOST_TEST(balloon.asValue(100, 355, 190)	== false);
 }
 
 BOOST_AUTO_TEST_CASE(constructor_test2)
 {
-	t_DirectivityBalloon balloon1("speaker-test2.txt");
+	t_DirectivityBalloon balloon("speaker-test2.txt");
+
+	BOOST_TEST(balloon.asDataForFrequency(40)		== true);
+	BOOST_TEST(balloon.asDataForFrequency(2000)		== true);
+	BOOST_TEST(balloon.asDataForFrequency(20000)	== false);
+
+	BOOST_TEST(balloon.asValue(100, 45, 90)		== true);
+	BOOST_TEST(balloon.asValue(100, 355, 170)	== true);
+
+	BOOST_TEST(balloon.asValue(205, 45, 90)		== false);
+	BOOST_TEST(balloon.asValue(205, 355, 170)	== false);
+	BOOST_TEST(balloon.asValue(100, 48, 90)		== false);
+	BOOST_TEST(balloon.asValue(100, 400, 170)	== false);
+	BOOST_TEST(balloon.asValue(100, 45, 2)		== false);
+	BOOST_TEST(balloon.asValue(100, 355, 190)	== false);
 }
 
+BOOST_AUTO_TEST_CASE(getValue_test)
+{
+	t_DirectivityBalloon balloon("speaker-test2.txt");
 
+	BOOST_TEST(balloon.getValue(50, 115, 30)	== -1.51);
+	BOOST_TEST(balloon.getValue(50, 115, 35)	== -1.71);
+	BOOST_TEST(balloon.getValue(2500, 260, 175) == -6.08);
+}
+
+BOOST_AUTO_TEST_CASE(getInterpolatedValue_test, *boost::unit_test::tolerance(boost::test_tools::fpc::percent_tolerance(1.0)))
+{
+	t_DirectivityBalloon balloon("speaker-test2.txt");
+
+	BOOST_TEST(balloon.getInterpolatedValue(40, 62, 29) == -1.48);
+	BOOST_TEST(balloon.getInterpolatedValue(40, 60, 27.5) == -1.45);
+}
