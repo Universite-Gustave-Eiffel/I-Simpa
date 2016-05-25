2f21dc7d678d7c7edb35121014a5541ed31c4f19
Austil <augustin@mrlacour.fr>
2016-05-25 17:09:07 +0200

    travis - switch to container infra to use cache

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -6,5 +6,5 @@ else
 	wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
 	tar -xjf wxWidgets-3.1.0.tar.bz2
 	mkdir $HOME/wxWidgets-install
-	cd wxWidgets-3.1.0 && ./configure --prefix=/wxWidgets-install && make && make install
+	cd wxWidgets-3.1.0 && ./configure --prefix=$HOME/wxWidgets-install && make && make install
 fi
