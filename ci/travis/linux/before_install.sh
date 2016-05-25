02a92dd88492680caaadaa823b9a2cbbe7aa54ee
Austil <augustin@mrlacour.fr>
2016-05-25 16:35:35 +0200

    travis - switch to container infra to use cache

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -1,35 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
-    cmake \
-    cmake-data \
-    python \
-    libboost-thread-dev \
-    libboost-random-dev \
-    libboost-system-dev \
-    libboost-filesystem-dev \
-    libboost-test-dev \
-	libboost-python-dev \
-	freeglut3-dev \
-	libxmu-dev \
-    libxi-dev \
-	swig \
-	libpng-dev \
-	libjpeg-dev \
-	libxxf86vm1 \
-	libxxf86vm-dev \
-	libxi-dev \
-	libxrandr-dev \
-	mesa-common-dev \
-	mesa-utils-extra \
-    libgl1-mesa-dev \
-    libglapi-mesa \
-    libgtk-3-dev
 
 if [ -d $HOME/wxWidgets-install ] ; then
-	echo "wxWidget already build (and in travis cache)"
+	echo "wxWidget already builded (and in travis cache)"
 else
 	wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
 	tar -xjf wxWidgets-3.1.0.tar.bz2
-	sudo mkdir $HOME/wxWidgets-install
-	cd wxWidgets-3.1.0 && ./configure --prefix=/wxWidgets-install && make && sudo make install
+	mkdir $HOME/wxWidgets-install
+	cd wxWidgets-3.1.0 && ./configure --prefix=/wxWidgets-install && make && make install
 fi
