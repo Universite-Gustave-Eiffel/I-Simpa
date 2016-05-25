86dd74ba6d3f6a6dc3ff827f6e7c79cdb986ef83
Austil <augustin@mrlacour.fr>
2016-05-25 14:52:11 +0200

    travis first try - fix - fix

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -25,7 +25,11 @@ sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
     libglapi-mesa \
     libgtk-3-dev
 
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
-tar -xjvf wxWidgets-3.1.0.tar.bz2
-sudo mkdir /wxWidgets-install
-cd wxWidgets-3.1.0 && ./configure --prefix=/wxWidgets-install && make && sudo make install
+if [ -d /wxWidgets-install ] ; then
+	echo "wxWidget already build (and in travis cache)"
+else
+	wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
+	tar -xjf wxWidgets-3.1.0.tar.bz2
+	sudo mkdir /wxWidgets-install
+	cd wxWidgets-3.1.0 && ./configure --prefix=/wxWidgets-install && make && sudo make install
+fi
