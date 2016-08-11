4bdc010cd9fc602f243e4a38a2e62fc2063c3be5
nicolas-f <github@nettrader.fr>
2016-08-11 11:33:59 +0200

    another try compile boost

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -4,6 +4,18 @@ export DEBIAN_FRONTEND=noninteractive
 export CXX="g++-4.8"
 export CC="gcc-4.8"
 
+if [ -d $HOME/boost-install/include ] ; then
+	echo "Boost already builded (and in travis cache)"
+else
+	cd
+	wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
+	tar -xjf boost_1_61_0.tar.bz2
+	mkdir $HOME/boost-install
+	echo "using gcc : 4.8 : gcc-4.8 ;" > ~/user-config.jam
+	cd  $HOME/boost_1_61_0 && ./bootstrap.sh link=static variant=release address-model=64 cxxflags="-std=c++11 -fPIC" boost.locale.icu=off --with-libraries=filesystem,system,test,regex,python,random,thread --prefix=$HOME/boost-install && ./b2 install
+fi
+
+
 
 if [ -d $HOME/wxWidgets-install/include ] ; then
 	echo "wxWidget already builded (and in travis cache)"
@@ -20,13 +32,3 @@ export PATH=$HOME/wxWidgets-install/bin/:$PATH
 echo "wxWidget version : "
 wx-config --version
 
-if [ -d $HOME/boost-install/include ] ; then
-	echo "Boost already builded (and in travis cache)"
-else
-	cd
-	wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2
-	tar -xjf boost_1_61_0.tar.bz2
-	mkdir $HOME/boost-install
-	echo "using gcc : 4.8 : gcc-4.8 ;" > ~/user-config.jam
-	cd  $HOME/boost_1_61_0 && ./bootstrap.sh link=static variant=release address-model=64 cxxflags="-std=c++11 -fPIC" boost.locale.icu=off --with-libraries=filesystem,system,test,regex,python,random,thread --prefix=$HOME/boost-install && ./b2 install
-fi
