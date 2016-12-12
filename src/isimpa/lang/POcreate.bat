7a6fa696e2becf5e3f7115fe631dac5ea62c0e15
nicolas-f <github@nettrader.fr>
2016-12-12 14:37:44 +0100

    transifex config

--- src/isimpa/lang/POcreate.bat
+++ src/isimpa/lang/POcreate.bat
@@ -2,6 +2,6 @@ cd ..
 
 dir /S /B *.c* *.h* > files.txt
 
-xgettext --keyword=_  --from-code=ISO-8859-15 -s --no-wrap  -no-hash --escape -ffiles.txt -olang\internat.po
+xgettext --keyword=_  --from-code=ISO-8859-15 -s --no-wrap  -no-hash --escape -ffiles.txt -olang\internat.pot
 
 pause
