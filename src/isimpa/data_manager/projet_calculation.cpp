dc36649f768b79514064ce4b6045b81760b3f916
nicolas-f <github@nettrader.fr>
2016-12-14 11:51:55 +0100

    About #7 Compute from begin propagation

--- src/isimpa/data_manager/projet_calculation.cpp
+++ src/isimpa/data_manager/projet_calculation.cpp
@@ -73,10 +73,10 @@ void MakeSchroederArray(const std::vector<std::vector<wxFloat32> >& srcArray,std
  */
 enum SUM_OPERATION
 {
-	SUM_OPERATION_X,
-	SUM_OPERATION_Y,
-	SUM_OPERATION_XY,
-	SUM_OPERATION_X2
+	SUM_OPERATION_X, // sum time
+	SUM_OPERATION_Y, // sum power
+	SUM_OPERATION_XY,// sum power * time
+	SUM_OPERATION_X2 // sum time²
 };
 
 wxFloat32 GetSumLimit(wxInt32 idBandeFreq,wxFloat32 fromTime,wxFloat32 toTime,const std::vector<wxFloat32>& timeTable,const std::vector<std::vector<wxFloat32> >& tab_wj, SUM_OPERATION operation=SUM_OPERATION_Y,wxInt32* counter=NULL)
@@ -110,6 +110,20 @@ struct LinearRegressionResult
 	wxFloat32 a;
 	wxFloat32 b;
 };
+
+wxFloat32 GetTimeDecay(wxFloat32 fromdB, const std::vector<wxFloat32>& timeTable, const std::vector<wxFloat32>& row_db) {
+	wxFloat32 timeDeb = timeTable[0];
+	wxFloat32 lastdB = row_db[0];
+	for (int idStep = 0; idStep<row_db.size(); idStep++)
+	{
+		if (abs(row_db[idStep] - lastdB) >= fromdB) {
+			break;
+		} else {
+			timeDeb = timeTable[idStep];
+		}
+	}
+	return timeDeb;
+}
 /**
  * A partir des bornes de décroissance en dB retourne les bornes correspondantes en ms
  */
@@ -265,12 +279,19 @@ formatGABE::GABE_Data_Float* Compute_ST_Param(wxFloat32 t1,wxFloat32 t2,wxFloat3
 {
 	formatGABE::GABE_Data_Float* newParameter=new formatGABE::GABE_Data_Float(tab_wj.size()+1);
 
+
 	//Pour chaque bande de fréquence
 	wxFloat32 sum=0.f;
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
+		double wt1 = wt0 + t1;
+		double wt2 = wt0 + t2;
+		double wt3 = wt0 + t3;
+
 		//Calcul de 0|te(p²(t))/te|+inf(p²(t))
-		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,t2,t3,timeTable,tab_wj)/GetSumLimit(idFreq,0.,t1,timeTable,tab_wj);
+		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq, wt2, wt3,timeTable,tab_wj)/GetSumLimit(idFreq, wt0, wt1,timeTable,tab_wj);
 
 		//Conversion en dB
 		wxFloat32 db_CurrentFreq=to_deciBel(wj_CurrentFreq);
@@ -298,8 +319,12 @@ formatGABE::GABE_Data_Float* Compute_C_Param(wxFloat32 te,const std::vector<wxFl
 	//Pour chaque bande de fréquence
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
+		double wte = wt0 + te;
+
 		//Calcul de 0|te(p²(t))/te|+inf(p²(t))
-		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,0.f,te,timeTable,tab_wj)/GetSumLimit(idFreq,te,-1.f,timeTable,tab_wj);
+		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,wt0,wte,timeTable,tab_wj)/GetSumLimit(idFreq,wte,-1.f,timeTable,tab_wj);
 
 		//Conversion en dB
 		wxFloat32 db_CurrentFreq=to_deciBel(wj_CurrentFreq);
@@ -327,8 +352,12 @@ formatGABE::GABE_Data_Float* Compute_D_Param(wxFloat32 te,const std::vector<wxFl
 	//Pour chaque bande de fréquence
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
+		double wte = wt0 + te;
+
 		//Calcul de 0|te(p²(t))/te|+inf(p²(t))
-		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,0.f,te,timeTable,tab_wj)/GetSumLimit(idFreq,0,-1.f,timeTable,tab_wj))*100.f;
+		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,wt0,wte,timeTable,tab_wj)/GetSumLimit(idFreq,wt0,-1.f,timeTable,tab_wj))*100.f;
 
 		if(idFreq!=tab_wj.size()-1)
 			sum+=wj_CurrentFreq;
@@ -353,9 +382,11 @@ formatGABE::GABE_Data_Float* Compute_TS_Param(const std::vector<wxFloat32>& time
 	//Pour chaque bande de fréquence
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
-		//Calcul de 0|t(p²(t))/0|+inf(p²(t))
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
 
-		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,0.f,-1.f,timeTable,tab_wj,SUM_OPERATION_XY)/GetSumLimit(idFreq,0,-1.f,timeTable,tab_wj,SUM_OPERATION_Y);
+		//Calcul de 0|t(p²(t))/0|+inf(p²(t))
+		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,wt0,-1.f,timeTable,tab_wj,SUM_OPERATION_XY)/GetSumLimit(idFreq,wt0,-1.f,timeTable,tab_wj,SUM_OPERATION_Y);
 
 		if(idFreq!=tab_wj.size()-1)
 			sum+=wj_CurrentFreq;
@@ -378,8 +409,13 @@ formatGABE::GABE_Data_Float* Compute_LEF_Param(const wxFloat32& t1,const wxFloat
 	//Pour chaque bande de fréquence
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
+		double wt1 = wt0 + t1;
+		double wt2 = wt0 + t2;
+
 		//Calcul
-		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,t1,t2,timeTable,tab_wjTheta,SUM_OPERATION_Y)/GetSumLimit(idFreq,0,t2,timeTable,tab_wj,SUM_OPERATION_Y)*100.f;
+		wxFloat32 wj_CurrentFreq=GetSumLimit(idFreq,wt1,wt2,timeTable,tab_wjTheta,SUM_OPERATION_Y)/GetSumLimit(idFreq,wt0,wt2,timeTable,tab_wj,SUM_OPERATION_Y)*100.f;
 		
 		if(idFreq!=tab_wj.size()-1)
 			sum+=wj_CurrentFreq;
@@ -444,8 +480,12 @@ formatGABE::GABE_Data_Float* Compute_LG_Param(const wxFloat32& t1,const wxFloat3
 	wxFloat32 sum=0.f;
 	for(int idFreq=0;idFreq<tab_wjthetasqr.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wjthetasqr[idFreq]);
+		double wt1 = wt0 + t1;
+		double wt2 = wt0 + t2;
 		//Calcul
-		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,t1,t2,timeTable,tab_wjthetasqr,SUM_OPERATION_Y))/(tab_wjsrc[idFreq]/(4*M_PI*100));
+		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,wt1,wt2,timeTable,tab_wjthetasqr,SUM_OPERATION_Y))/(tab_wjsrc[idFreq]/(4*M_PI*100));
 		
 		if(idFreq!=tab_wjthetasqr.size()-1)
 			sum+=wj_CurrentFreq;
@@ -474,8 +514,12 @@ formatGABE::GABE_Data_Float* Compute_G_Param(const wxFloat32& t1,const wxFloat32
 	wxFloat32 sum=0.f;
 	for(int idFreq=0;idFreq<tab_wj.size();idFreq++)
 	{
+		// Wave time
+		double wt0 = GetTimeDecay(EPSILON, timeTable, tab_wj[idFreq]);
+		double wt1 = wt0 + t1;
+		double wt2 = wt0 + t2;
 		//Calcul
-		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,t1,t2,timeTable,tab_wj,SUM_OPERATION_Y))/(tab_wjsrc[idFreq]/(4*M_PI*100));
+		wxFloat32 wj_CurrentFreq=(GetSumLimit(idFreq,wt1,wt2,timeTable,tab_wj,SUM_OPERATION_Y))/(tab_wjsrc[idFreq]/(4*M_PI*100));
 		newParameter->Set(idFreq,to_deciBel(wj_CurrentFreq));
 		if(idFreq!=tab_wj.size()-1)
 			sum+=to_deciBel(wj_CurrentFreq);
