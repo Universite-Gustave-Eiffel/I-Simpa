35465208f51d10593b5aed494ba87b914ea4f43d
nicolas-f <github@nettrader.fr>
2017-04-07 16:18:13 +0200

    Simplify faces export and add source positions

--- core/md_octave/README.txt
+++ core/md_octave/README.txt
@@ -1,17 +1,21 @@
 Input files for Octave computation core
 
+scene_sources.txt
+Source position and power for each third octave band
+posX, posY, posZ, 50, 63,80,100,125,160,200,250,315,400,500,630,800,1000,1250,1600,2000,3150,4000,5000,6300,8000,10000,12500,16000,20000
+
+
 scene_nodes.txt
 Vertices of the tetrahedral mesh
 x y z
 
 scene_elements.txt
-Vertices index (0-n) of tetrahedral elements
-A, B, C, D, IdVolume
+Vertices index (1-n) of tetrahedral elements and the volume identifier.
+A, B, C, D, idVolume
 
-scene_tetrafaces.txt
-Material identifier of each tetrahedral faces and the volume identifier.
-The face order follow the vertices order ex: I(B,C,D) J(A,C,D) K(A,B,D) L(A,B,C)
-I, J, K, L, idVolume
+scene_faces.txt
+Material identifier of each faces
+A, B, C, materialId
 
 scene_materials_absorption.txt
 Material Identifier and absorption value(0-1) for each third octave band
--- core/md_octave/md_octave.py
+++ core/md_octave/md_octave.py
@@ -29,13 +29,11 @@ def which(program):
 
     return None
     
-def getMaterial(marker, modelImport):
-    if marker > 0:
-        return modelImport.faces[marker].idMat
-    else:
-        return -1
+def processFace(tetraface, modelImport, fileOut):
+    if tetraface.marker >= 0:
+        fileOut.write('{0:>6} {1:>6} {2:>6} {3:>6}'.format(*(tetraface.vertices[0], tetraface.vertices[1], tetraface.vertices[2], modelImport.faces[tetraface.marker].idMat)) + "\n")
         
-def writeInputFiles(cbinpath,cmbinpath,materials, outfolder):
+def writeInputFiles(cbinpath,cmbinpath,materials, sources_lst, outfolder):
     # Import 3D model
     # This model contains the associated material link to the XML file
     modelImport = ls.ioModel()
@@ -57,12 +55,15 @@ def writeInputFiles(cbinpath,cmbinpath,materials, outfolder):
     # Write elements file
     with open(outfolder+"scene_elements.txt", "w") as f:
         for tetra in meshImport.tetrahedres:
-            f.write('{0:>6} {1:>6} {2:>6} {3:>6}'.format(*(tetra.vertices[0]+1, tetra.vertices[1]+1, tetra.vertices[2]+1, tetra.vertices[3]+1)) + "\n")
+            f.write('{0:>6} {1:>6} {2:>6} {3:>6} {4:>6}'.format(*(tetra.vertices[0]+1, tetra.vertices[1]+1, tetra.vertices[2]+1, tetra.vertices[3]+1, tetra.idVolume)) + "\n")
         
     # Write tetra face file
-    with open(outfolder+"scene_tetrafaces.txt", "w") as f:
+    with open(outfolder+"scene_faces.txt", "w") as f:
         for tetra in meshImport.tetrahedres:
-            f.write('{0:>6} {1:>6} {2:>6} {3:>6} {4:>6}'.format(*(getMaterial(tetra.getFace(0).marker, modelImport), getMaterial(tetra.getFace(1).marker, modelImport), getMaterial(tetra.getFace(2).marker, modelImport), getMaterial(tetra.getFace(3).marker, modelImport), tetra.idVolume)) + "\n")
+            processFace(tetra.getFace(0), modelImport, f)
+            processFace(tetra.getFace(1), modelImport, f)
+            processFace(tetra.getFace(2), modelImport, f)
+            processFace(tetra.getFace(3), modelImport, f)
 
     # Write boundary material file
     with open(outfolder+"scene_materials_absorption.txt", "w") as f:
@@ -84,6 +85,15 @@ def writeInputFiles(cbinpath,cmbinpath,materials, outfolder):
             #end of line
             f.write("\n")
     
+    # Write source position and power files
+    with open(outfolder+"scene_sources.txt", "w") as f:
+        for src in sources_lst:
+            f.write('{0:>15} {1:>15} {2:>15} '.format(src.pos[0], src.pos[1], src.pos[2]))
+            # for each frequency band
+            for spl in src.db:
+                f.write(' {0:>6.4g}'.format(spl))            
+            #end of line
+            f.write("\n")
     
     
 def main(callOctave=True):
@@ -94,20 +104,20 @@ def main(callOctave=True):
     coreconf=cc.coreConfig(sys.argv[1])
     outputdir=coreconf.paths["workingdirectory"]
     #Translation CBIN 3D model and 3D tetrahedra mesh into Octave input files
-    writeInputFiles(outputdir+coreconf.paths["modelName"], outputdir+coreconf.paths["tetrameshFileName"], coreconf.materials ,outputdir)
+    writeInputFiles(outputdir+coreconf.paths["modelName"], outputdir+coreconf.paths["tetrameshFileName"], coreconf.materials, coreconf.sources_lst ,outputdir)
+    # Copy octave script to working dir
+    matscript_folder = os.path.join(os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir)), "script")
+    files = glob.iglob(os.path.join(matscript_folder, "*.m"))
+    print(os.path.join(matscript_folder, "*.m"))
+    for filep in files:
+        if os.path.isfile(filep):
+            shutil.copy2(filep, outputdir)
     if callOctave:
         # Check if octave program are accessible in path
         octave = which("octave.bat")
         if octave is None:
-            print("Octave program not in system path", file=sys.stderr)
+            print("Octave program not in system path, however input files are created", file=sys.stderr)
         else:
-            # Copy octave script to working dir
-            matscript_folder = os.path.join(os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir)), "script")
-            files = glob.iglob(os.path.join(matscript_folder, "*.m"))
-            print(os.path.join(matscript_folder, "*.m"))
-            for filep in files:
-                if os.path.isfile(filep):
-                    shutil.copy2(filep, outputdir)
             command = ["octave.bat","--persist", outputdir+"MVCEF3D.m"]
             print("Run " + " ".join(command))
             call(command, cwd=outputdir, shell=True)
