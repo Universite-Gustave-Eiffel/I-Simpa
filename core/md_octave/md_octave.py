78df1acefa4989e7b3ffdb711366652ab0df6afd
nicolas-f <github@nettrader.fr>
2017-05-22 16:37:21 +0200

    Output nodes thare in between two rooms

--- core/md_octave/md_octave.py
+++ core/md_octave/md_octave.py
@@ -28,42 +28,56 @@ def which(program):
                 return exe_file
 
     return None
-    
-def processFace(tetraface, modelImport, fileOut):
+
+def processFace(tetraface, modelImport, sharedVertices, fileOut):
     if tetraface.marker >= 0:
         fileOut.write('{0:>6} {1:>6} {2:>6} {3:>6}'.format(*(tetraface.vertices[0] + 1, tetraface.vertices[1] + 1, tetraface.vertices[2] + 1, modelImport.faces[tetraface.marker].idMat)) + "\n")
-        
+        if tetraface.neighbor >= 0:
+            sharedVertices.add(tetraface.vertices[0] + 1)
+            sharedVertices.add(tetraface.vertices[1] + 1)
+            sharedVertices.add(tetraface.vertices[2] + 1)
+
 def writeInputFiles(cbinpath,cmbinpath,materials, sources_lst, outfolder):
     # Import 3D model
     # This model contains the associated material link to the XML file
+
+    # XML volume index to octave index
+    idVolumeIndex = {}
+    sharedVertices = set()
+
+
     modelImport = ls.ioModel()
     if not ls.CformatBIN().ImportBIN( modelImport, cbinpath):
         print("Error can not load %s model !\n" % cbinpath)
-        return -1    
-    
+        return -1
+
     # Import 3D mesh file builded from tetgen output
     meshImport = ls.CMBIN().LoadMesh(cmbinpath)
     if not meshImport:
         print("Error can not load %s mesh model !\n" % cmbinpath)
         return -1
-        
+
     # Write NODES file
     with open(outfolder+"scene_nodes.txt", "w") as f:
         for node in meshImport.nodes:
             f.write('{0:>15} {1:>15} {2:>15}'.format(*(node[0], node[1], node[2])) + "\n")
-        
+
     # Write elements file
     with open(outfolder+"scene_elements.txt", "w") as f:
         for tetra in meshImport.tetrahedres:
-            f.write('{0:>6} {1:>6} {2:>6} {3:>6} {4:>6}'.format(*(tetra.vertices[0]+1, tetra.vertices[1]+1, tetra.vertices[2]+1, tetra.vertices[3]+1, tetra.idVolume)) + "\n")
-        
+            volindex = idVolumeIndex.get(tetra.idVolume)
+            if volindex is None:
+                volindex = len(idVolumeIndex) + 1
+                idVolumeIndex[tetra.idVolume]= len(idVolumeIndex) + 1
+            f.write('{0:>6} {1:>6} {2:>6} {3:>6} {4:>6}'.format(*(tetra.vertices[0]+1, tetra.vertices[1]+1, tetra.vertices[2]+1, tetra.vertices[3]+1, volindex)) + "\n")
+
     # Write tetra face file
     with open(outfolder+"scene_faces.txt", "w") as f:
         for tetra in meshImport.tetrahedres:
-            processFace(tetra.getFace(0), modelImport, f)
-            processFace(tetra.getFace(1), modelImport, f)
-            processFace(tetra.getFace(2), modelImport, f)
-            processFace(tetra.getFace(3), modelImport, f)
+            processFace(tetra.getFace(0), modelImport,sharedVertices, f)
+            processFace(tetra.getFace(1), modelImport,sharedVertices, f)
+            processFace(tetra.getFace(2), modelImport,sharedVertices, f)
+            processFace(tetra.getFace(3), modelImport,sharedVertices, f)
 
     # Write boundary material file
     with open(outfolder+"scene_materials_absorption.txt", "w") as f:
@@ -71,31 +85,35 @@ def writeInputFiles(cbinpath,cmbinpath,materials, sources_lst, outfolder):
             f.write('{0:>6} '.format(xmlid))
             # for each frequency band
             for freqAbs in mat["q"]:
-                f.write(' {0:>6.2g}'.format(freqAbs))            
+                f.write(' {0:>6.2g}'.format(freqAbs))
             #end of line
             f.write("\n")
-            
+
     # Write boundary material transmission file
     with open(outfolder+"scene_materials_transmission.txt", "w") as f:
         for xmlid, mat in materials.iteritems():
             f.write('{0:>6} '.format(xmlid))
             # for each frequency band
             for freqTransm in mat["g"]:
-                f.write(' {0:>6.2g}'.format(freqTransm))            
+                f.write(' {0:>6.2g}'.format(freqTransm))
             #end of line
             f.write("\n")
-    
+
     # Write source position and power files
     with open(outfolder+"scene_sources.txt", "w") as f:
         for src in sources_lst:
             f.write('{0:>15} {1:>15} {2:>15} '.format(src.pos[0], src.pos[1], src.pos[2]))
             # for each frequency band
             for spl in src.db:
-                f.write(' {0:>6.4g}'.format(spl))            
+                f.write(' {0:>6.4g}'.format(spl))
             #end of line
             f.write("\n")
-    
-    
+
+    # Write shared vertices index
+    with open(outfolder+"scene_shared_vertices.txt", "w") as f:
+        for ptindex in sharedVertices:
+            f.write(str(ptindex) + "\n")
+
 def main(callOctave=True):
 
     #find core folder
