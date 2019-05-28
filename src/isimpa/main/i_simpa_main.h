01504bbc7d05de89dc6a8738458dcb20f4579f8a
wbinek <wojciech.binek@gmail.com>
2019-05-28 10:08:47 +0200

    Fix language selection

--- src/isimpa/main/i_simpa_main.h
+++ src/isimpa/main/i_simpa_main.h
@@ -458,7 +458,7 @@ class ISimpaApp : public wxApp
 				choosenLanguage=(wxLanguage)MainUiFrame::AskApplicationLanguage(choosenLanguage);
 			}
 			lang.Init(choosenLanguage, wxLOCALE_LOAD_DEFAULT);
-			lang.AddCatalog("internat",wxLANGUAGE_DEFAULT,"");
+			lang.AddCatalog("internat", wxLANGUAGE_ENGLISH,"");
 
 
             if(ApplicationConfiguration::GetFileConfig()->Read("interface/appdata",&strConf)) {
