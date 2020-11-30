6869daf6613cad55397cb31c346aa95fc66a1f6f
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2020-11-30 17:46:06 +0100

    isimpa version to python3

--- currentRelease/SystemScript/check_version/__init__.py
+++ currentRelease/SystemScript/check_version/__init__.py
@@ -1,28 +1,33 @@
 # -*- coding: UTF-8 -*-
 import uictrl as ui
 import uilocale
-import urllib2
+import urllib.request
 import os
 import re
 
+_ = uilocale.InstallUiModule(ui.application.getapplicationpath()["systemscript"]+"check_version"+os.sep,ui.application.getlocale())
 
-_=uilocale.InstallUiModule(ui.application.getapplicationpath()["systemscript"]+"check_version"+os.sep,ui.application.getlocale())
+
+def versionToCode(major, minor, revision):
+    return major * 1000000 + minor * 1000 + revision
+
 
 def check_version():
-    visimpa = map(int, ui.application.getversion().split("."))[:3]
+    visimpa = list(map(int, ui.application.getversion().split(".")))[:3]
+    visimpa = versionToCode(visimpa[0], visimpa[1], visimpa[2])
 
-    # https://raw.githubusercontent.com/Ifsttar/I-Simpa/master/CMakeLists.txt
-    u = urllib2.urlopen("https://raw.githubusercontent.com/Ifsttar/I-Simpa/master/CMakeLists.txt", timeout=5)
-    p = re.compile("^project \(isimpa VERSION (\d+\.)?(\d+\.)?(\*|\d+)")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/Ifsttar/I-Simpa/master/CMakeLists.txt") as response:
+        html = response.read().decode("utf-8")
+        p = re.compile("^project \(isimpa VERSION (\d+\.)?(\d+\.)?(\*|\d+)")
 
-    for l in u.readlines():
-        r = p.match(l)
-        if r is not None:
-            major = int(r.group(1)[:1])
-            minor = int(r.group(2)[:1])
-            revision = int(r.group(3))
-            if major > visimpa[0] or minor > visimpa[1] or revision > visimpa[2]:
-                    ui.application.loginfo(_("A new version of I-Simpa is available, please look at https://github.com/Ifsttar/I-Simpa/releases/latest"))
-            return major, minor, revision
-    
+        for line in html.split("\n"):
+            r = p.match(line)
+            if r is not None:
+                major = int(r.group(1)[:1])
+                minor = int(r.group(2)[:1])
+                revision = int(r.group(3))
+                if visimpa < versionToCode(major, minor, revision):
+                        ui.application.loginfo(_("A new version of I-Simpa is available, please look at https://github.com/Ifsttar/I-Simpa/releases/latest"))
+                return major, minor, revision
+    return 0, 0, 0
 
