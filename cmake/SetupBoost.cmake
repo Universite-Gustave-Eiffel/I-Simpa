9f07dd6d1741228dcad9ca314374daa4d0718263
nicolas-f <github@nettrader.fr>
2024-10-03 16:49:00 +0200

    enable boost python for cmake cpm

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -5,7 +5,7 @@ macro(SetupBoost)
           VERSION 1.86.0
           URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
           URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
-          OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;python\\\;numeric/conversion\\\;test" # Note the escapes!
+          OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_ENABLE_PYTHON ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;python\\\;numeric/conversion\\\;test" # Note the escapes!
         )
     else()
       find_package (Threads)
