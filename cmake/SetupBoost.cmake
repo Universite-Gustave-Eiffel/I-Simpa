7b3faa61538145a3980a335f7b5d275bf1241918
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2025-10-16 15:41:01 +0200

    flatpak modules for cpm and boost

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -1,20 +1,29 @@
 macro(SetupBoost)
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        CPMAddPackage(
-          NAME Boost
-          VERSION 1.86.0
-          URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
-          URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
-          OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_ENABLE_PYTHON ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;python\\\;numeric/conversion\\\;test" # Note the escapes!
-        )
-    else()
-      find_package (Threads)
-        CPMAddPackage(
-          NAME Boost
-          VERSION 1.86.0
-          URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
-          URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
-          OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_ENABLE_PYTHON ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;thread\\\;chrono\\\;test\\\;date_time\\\;python\\\;numeric/conversion" # Note the escapes!
-        )
+    find_package(Boost 1.86.0 COMPONENTS system algorithm random filesystem regex thread chrono date_time python)
+
+    if(NOT Boost_FOUND)
+        message(STATUS "Boost not found")
+        message(STATUS "Boost_INCLUDE_DIRS=${Boost_INCLUDE_DIRS}")
+        message(STATUS "Boost_LIBRARIES=${Boost_LIBRARIES}")
+        message(STATUS "Boost_VERSION=${Boost_VERSION}")
+
+        if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+            CPMAddPackage(
+              NAME Boost
+              VERSION 1.86.0
+              URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
+              URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
+              OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_ENABLE_PYTHON ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;python\\\;numeric/conversion\\\;test" # Note the escapes!
+            )
+        else()
+          find_package (Threads)
+            CPMAddPackage(
+              NAME Boost
+              VERSION 1.86.0
+              URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
+              URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
+              OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_ENABLE_PYTHON ON" "BOOST_INCLUDE_LIBRARIES system\\\;algorithm\\\;random\\\;filesystem\\\;regex\\\;thread\\\;chrono\\\;test\\\;date_time\\\;python\\\;numeric/conversion" # Note the escapes!
+            )
+        endif()
     endif()
 endmacro(SetupBoost)
