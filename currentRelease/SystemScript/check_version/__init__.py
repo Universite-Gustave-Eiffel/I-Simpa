7fa4d83a3fd907f9a036030f0816df19aa34e35e
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2025-11-06 10:01:35 +0100

    fix flatpak build fix missing requests package and check version python script

--- currentRelease/SystemScript/check_version/__init__.py
+++ currentRelease/SystemScript/check_version/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
 import uictrl as ui
 import uilocale
-import urllib.request
 import os
 import re
+import requests
 
 _ = uilocale.InstallUiModule(ui.application.getapplicationpath()["systemscript"]+"check_version"+os.sep,ui.application.getlocale())
 
@@ -15,19 +15,11 @@ def versionToCode(major, minor, revision):
 def check_version():
     visimpa = list(map(int, ui.application.getversion().split(".")))[:3]
     visimpa = versionToCode(visimpa[0], visimpa[1], visimpa[2])
-
-    with urllib.request.urlopen("https://raw.githubusercontent.com/Ifsttar/I-Simpa/master/CMakeLists.txt") as response:
-        html = response.read().decode("utf-8")
-        p = re.compile("^project \(isimpa VERSION (\d+\.)?(\d+\.)?(\*|\d+)")
-
-        for line in html.split("\n"):
-            r = p.match(line)
-            if r is not None:
-                major = int(r.group(1)[:1])
-                minor = int(r.group(2)[:1])
-                revision = int(r.group(3))
-                if visimpa < versionToCode(major, minor, revision):
-                        ui.application.loginfo(_("A new version of I-Simpa is available, please look at https://github.com/Ifsttar/I-Simpa/releases/latest"))
-                return major, minor, revision
-    return 0, 0, 0
+    response = requests.get("https://api.github.com/repos/Universite-Gustave-Eiffel/I-Simpa/releases/latest")
+    data = response.json()
+    latest_tag = data['tag_name']
+    major, minor, revision = map(int, latest_tag[1:].split("."))
+    if visimpa < versionToCode(major, minor, revision):
+            ui.application.loginfo(_("A new version of I-Simpa is available, please look at https://github.com/Universite-Gustave-Eiffel/I-Simpa/releases/latest"))
+    return major, minor, revision
 
