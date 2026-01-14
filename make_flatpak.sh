077d9ed5a9766abf3bdc0e8637febac025329f16
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2026-01-14 09:26:20 +0100

    Improve Flatpak setup and add installation instructions
    - Remove Flatpak upload step from GitHub workflow.
    - Update README with installation instructions for Windows, Linux, and macOS.
    - Enhance Flatpak bundle creation for offline installations.
    - Add `.flatpak` files to `.gitignore`.
    - Specify `runtime-repo` in Flatpak manifest.
    - Macos github action

--- make_flatpak.sh
+++ make_flatpak.sh
@@ -4,4 +4,7 @@ flatpak-builder --user --install --force-clean build org.noise_planet.i-simpa.ym
 # flatpak run org.noise_planet.i-simpa
 
 # make flatpak bundle
-# flatpak build-bundle .flatpak-builder/cache isimpa.flatpak org.noise_planet.i-simpa
+# flatpak build-bundle .flatpak-builder/cache isimpa.flatpak org.noise_planet.i-simpa --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
+
+# make independent flapak for offline install
+# flatpak create-usb --user ./offline org.noise_planet.i-simpa
